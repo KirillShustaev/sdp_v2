30fa5963b4a090cff6b72796c8533f8151228143
Kirill Shustaev <kirill.shustaev@yandex.ru>
2025-02-04 17:14:49 +0300

    hero_and_spec

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,5 @@ let headerForm = document.querySelector(".header__form")
 let headerSearch = document.querySelector(".header__search")
 
 headerSearch.addEventListener("input", ()=> {
-    headerForm.classList.add("noneLoop")
+    headerForm.classList.toggle("noneLoop")
 })
