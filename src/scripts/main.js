5a39aafb6038177bdb50a6c0483c797cefc38db6
Kirill Shustaev <kirill.shustaev@yandex.ru>
2025-02-13 15:30:08 +0300

    catalog

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
+let catalogBtnSwiper = document.querySelectorAll(".catalog__btnSwiper")
+
+catalogBtnSwiper.forEach(e => {
+    e.addEventListener("click", () => {
+        for(elem of catalogBtnSwiper) {
+            elem.classList.remove("catalogBtnActive")
+        }
+        e.classList.add("catalogBtnActive")
+    })
+
+})
+
+//
 let headerForm = document.querySelector(".header__form")
 let headerSearch = document.querySelector(".header__search")
 
@@ -18,6 +31,17 @@ incrementButtons.forEach(button => {
         counterElement.textContent = count;
     });
 });
+
+swiper = new Swiper('.catalog__swiper', {
+	slidesPerGroup: 1,
+    slidesPerView: 1,
+	spaceBetween: 20,
+	loop: true,
+	navigation: {
+		nextEl: '.catalog__btnSwiper_1',
+		prevEl: '.catalog__btnSwiper_2',
+		},
+  })
 //
 let phoneInput = document.getElementById('phone');
 let phoneMask = IMask(phoneInput, {
